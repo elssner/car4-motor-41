e320700cdf2fa3f001e18b836b33801529e659b7
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-12-30 16:30:44 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
 radio.onReceivedNumber(function (receivedNumber) {
     btLaufzeit = input.runningTime()
     qwiicmotor.setReceivedNumber(receivedNumber)
+    if (!(btConnected) && (qwiicmotor.getReceivedNumber(NumberFormat.UInt8LE, qwiicmotor.eOffset.z0) == 128 && qwiicmotor.getReceivedNumber(NumberFormat.UInt8LE, qwiicmotor.eOffset.z1) == 90)) {
+        bit.comment("einmalig nach neu connected")
+        btConnected = true
+        qwiicmotor.controlRegister(qwiicmotor.qwiicmotor_eADDR(qwiicmotor.eADDR.Motor_x5D), qwiicmotor.eControl.DRIVER_ENABLE, true)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.setLedColor(0x00ff00)
+    }
     bit.comment("0 Motor 0..128..255")
+    MotorSteuerung(qwiicmotor.getReceivedNumber(NumberFormat.UInt8LE, qwiicmotor.eOffset.z0))
     bit.comment("1 Servo 0..45..135")
-    i2cSchleife(true, qwiicmotor.getReceivedNumber(NumberFormat.UInt8LE, qwiicmotor.eOffset.z0), qwiicmotor.getReceivedNumber(NumberFormat.UInt8LE, qwiicmotor.eOffset.z1))
+    ServoSteuerung(qwiicmotor.getReceivedNumber(NumberFormat.UInt8LE, qwiicmotor.eOffset.z1))
 })
 function MotorSteuerung (pMotorPower: number) {
     if (!(btConnected)) {
@@ -17,9 +25,23 @@ function MotorSteuerung (pMotorPower: number) {
         qwiicmotor.controlRegister(qwiicmotor.qwiicmotor_eADDR(qwiicmotor.eADDR.Motor_x5D), qwiicmotor.eControl.DRIVER_ENABLE, true)
     }
 }
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    basic.showNumber(Math.round(Math.map(255, 0, 255, 135, 45)))
-})
+function nichts1 () {
+    bit.comment("Überwachung Bluetooth")
+    if (input.runningTime() - btLaufzeit < 1000) {
+        bit.comment("Bluetooth ist verbunden: 'wenn Zahl empfangen' ruft i2cSchleife auf")
+    } else if (input.runningTime() - btLaufzeit > 60000) {
+        bit.comment("nach 1 Minute ohne Bluetooth Relais aus schalten")
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    } else {
+        bit.comment("zwischen 1 Sekunde und 1 Minute ohne Bluetooth: Standby und blau blinken")
+        i2cSchleife(false, 128, 90)
+        if (Math.trunc(input.runningTime() / 1000) % 2 == 1) {
+            basic.setLedColor(0x0000ff)
+        } else {
+            basic.turnRgbLedOff()
+        }
+    }
+}
 function zeigeStatus () {
 	
 }
@@ -80,18 +102,24 @@ radio.setGroup(240)
 pins.servoWritePin(AnalogPin.C17, 96)
 loops.everyInterval(500, function () {
     bit.comment("Überwachung Bluetooth")
-    if (input.runningTime() - btLaufzeit < 1000) {
-        bit.comment("Bluetooth ist verbunden: 'wenn Zahl empfangen' ruft i2cSchleife auf")
-    } else if (input.runningTime() - btLaufzeit > 60000) {
+    if (input.runningTime() - btLaufzeit > 60000) {
         bit.comment("nach 1 Minute ohne Bluetooth Relais aus schalten")
         pins.digitalWritePin(DigitalPin.P0, 0)
-    } else {
+    } else if (input.runningTime() - btLaufzeit > 1000) {
         bit.comment("zwischen 1 Sekunde und 1 Minute ohne Bluetooth: Standby und blau blinken")
-        i2cSchleife(false, 128, 90)
+        if (btConnected) {
+            bit.comment("einmalig nach neu disconnected")
+            btConnected = false
+            qwiicmotor.controlRegister(qwiicmotor.qwiicmotor_eADDR(qwiicmotor.eADDR.Motor_x5D), qwiicmotor.eControl.DRIVER_ENABLE, false)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+        }
+        bit.comment("dauerhaft wenn disconnected")
         if (Math.trunc(input.runningTime() / 1000) % 2 == 1) {
             basic.setLedColor(0x0000ff)
         } else {
             basic.turnRgbLedOff()
         }
+    } else {
+        bit.comment("Bluetooth ist verbunden: 'wenn Zahl empfangen' ruft i2cSchleife auf")
     }
 })
