b91f34a33dc956c9ac9c3f2429e3b4f12185888c
GTA Calliope <73078489+elssner@users.noreply.github.com>
2024-01-01 15:58:37 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,6 @@ radio.onReceivedNumber(function (receivedNumber) {
         bit.comment("einmalig nach neu connected")
         btConnected = true
         qwiicmotor.controlRegister(qwiicmotor.qwiicmotor_eADDR(qwiicmotor.eADDR.Motor_x5D), qwiicmotor.eControl.DRIVER_ENABLE, true)
-        pins.digitalWritePin(DigitalPin.P1, 1)
         basic.setLedColor(0x00ff00)
     } else if (btConnected) {
         if (iFahrstrecke == 0) {
@@ -31,15 +30,23 @@ function MotorSteuerung (pMotorPower: number, pFahrstrecke: number) {
         iFahrstrecke = pFahrstrecke
         iEncoder = 0
         iMotor = pMotorPower
-        qwiicmotor.writeRegister(qwiicmotor.qwiicmotor_eADDR(qwiicmotor.eADDR.Motor_x5D), qwiicmotor.qwiicmotor_eRegister(qwiicmotor.eRegister.MB_DRIVE), iMotor)
+        qwiicmotor.writeRegister(qwiicmotor.qwiicmotor_eADDR(qwiicmotor.eADDR.Motor_x5D), qwiicmotor.qwiicmotor_eRegister(qwiicmotor.eRegister.MA_DRIVE), iMotor)
         bit.comment("Motor Stop im Impuls-Zähler")
     } else if (iMotor != pMotorPower) {
         bit.comment("connected und nur wenn von Sender empfangener Wert geändert")
         iMotor = pMotorPower
-        qwiicmotor.writeRegister(qwiicmotor.qwiicmotor_eADDR(qwiicmotor.eADDR.Motor_x5D), qwiicmotor.qwiicmotor_eRegister(qwiicmotor.eRegister.MB_DRIVE), iMotor)
+        qwiicmotor.writeRegister(qwiicmotor.qwiicmotor_eADDR(qwiicmotor.eADDR.Motor_x5D), qwiicmotor.qwiicmotor_eRegister(qwiicmotor.eRegister.MA_DRIVE), iMotor)
     }
 }
-pins.onPulsed(DigitalPin.P3, PulseValue.Low, function () {
+function zeigeStatus () {
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 15, lcd16x2rgb.lcd16x2_text("" + bit.formatText(iMotor, 3, bit.eAlign.right) + bit.formatText(iServo, 4, bit.eAlign.right) + bit.formatText(iFahrstrecke, 4, bit.eAlign.right) + bit.formatText(iEncoder, 5, bit.eAlign.right)))
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 0, 7, "" + bit.formatText(Helligkeit(pins.analogReadPin(AnalogPin.C4)), 3, bit.eAlign.right) + "Welt")
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 8, 15, "" + bit.formatText(bit.roundWithPrecision(wattmeter.get_bus_voltage_V(wattmeter.wattmeter_eADDR(wattmeter.eADDR.Watt_x45)), 1), 3, bit.eAlign.right) + "V" + bit.formatText(wattmeter.get_current_mA(wattmeter.wattmeter_eADDR(wattmeter.eADDR.Watt_x45)), 4, bit.eAlign.right))
+}
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
+pins.onPulsed(DigitalPin.C8, PulseValue.Low, function () {
     bit.comment("Encoder 63.3 Impulse pro U/Motorwelle")
     if (iMotor >= 128) {
         iEncoder += 1
@@ -51,13 +58,9 @@ pins.onPulsed(DigitalPin.P3, PulseValue.Low, function () {
         MotorSteuerung(128, 0)
     }
 })
-function zeigeStatus () {
-    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 15, lcd16x2rgb.lcd16x2_text("" + bit.formatText(iMotor, 3, bit.eAlign.right) + bit.formatText(iServo, 4, bit.eAlign.right) + bit.formatText(iFahrstrecke, 4, bit.eAlign.right) + bit.formatText(iEncoder, 5, bit.eAlign.right)))
-    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 8, 15, "" + bit.formatText(bit.roundWithPrecision(wattmeter.get_bus_voltage_V(wattmeter.wattmeter_eADDR(wattmeter.eADDR.Watt_x45)), 1), 3, bit.eAlign.right) + "V" + bit.formatText(wattmeter.get_current_mA(wattmeter.wattmeter_eADDR(wattmeter.eADDR.Watt_x45)), 4, bit.eAlign.right))
+function Helligkeit (pHelligkeit: number) {
+    return pHelligkeit
 }
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-})
 function Konfiguration () {
     bit.comment("P0 Grove Relay; P1 RB LED (DRIVER_ENABLE)")
     bit.comment("P2 frei; P3 Encoder")
@@ -70,7 +73,7 @@ function ServoSteuerung (pWinkel: number) {
     } else if (iServo != pWinkel) {
         bit.comment("connected und Wert geändert")
         iServo = pWinkel
-        pins.servoWritePin(AnalogPin.C17, iServo + 8)
+        pins.servoWritePin(AnalogPin.P1, iServo + 8)
         return true
     } else {
         return true
@@ -91,8 +94,8 @@ btConnected = false
 btLaufzeit = input.runningTime()
 iFahrstrecke = 0
 radio.setGroup(240)
-pins.servoWritePin(AnalogPin.C17, 96)
-pins.setPull(DigitalPin.P3, PinPullMode.PullUp)
+pins.servoWritePin(AnalogPin.P1, 96)
+pins.setPull(DigitalPin.C8, PinPullMode.PullUp)
 loops.everyInterval(800, function () {
     bit.comment("Überwachung Bluetooth")
     if (input.runningTime() - btLaufzeit > 60000) {
@@ -103,7 +106,6 @@ loops.everyInterval(800, function () {
         bit.comment("einmalig nach neu disconnected")
         btConnected = false
         qwiicmotor.controlRegister(qwiicmotor.qwiicmotor_eADDR(qwiicmotor.eADDR.Motor_x5D), qwiicmotor.eControl.DRIVER_ENABLE, false)
-        pins.digitalWritePin(DigitalPin.P1, 0)
     } else if (!(btConnected)) {
         bit.comment("dauerhaft wenn disconnected")
         zeigeStatus()
