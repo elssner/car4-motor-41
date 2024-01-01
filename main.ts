7ad73333fa4e193b78c1a3f0729074aee3b1ec75
GTA Calliope <73078489+elssner@users.noreply.github.com>
2024-01-01 17:45:04 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -62,9 +62,11 @@ function Helligkeit (pHelligkeit: number) {
     return pHelligkeit
 }
 function Konfiguration () {
-    bit.comment("P0 Grove Relay; P1 RB LED (DRIVER_ENABLE)")
-    bit.comment("P2 frei; P3 Encoder")
-    bit.comment("P16 Ultraschall; P17 Servo")
+    bit.comment("P0 Grove Relay; P1 Fototransistor")
+    bit.comment("P2 Encoder; P3 Buzzer")
+    bit.comment("P4 Servo; ")
+    bit.comment("P5 Draht blau; P6 gelb")
+    bit.comment("P7 Licht; P8 Ultraschall")
     bit.comment("5 Erweiterungen: Funk; BIT; LCD 16x2; Motor; Wattmeter")
 }
 function ServoSteuerung (pWinkel: number) {
